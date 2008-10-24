4abef304f37559f9d6d87ddc52610be2246494f6
CLR <clr@port49.com>
2008-10-24 10:45:09 -0400

    -- adding formatted named routes back in for recognition only --

--- lib/restful_mapper.rb
+++ lib/restful_mapper.rb
@@ -8,6 +8,10 @@ module ActionController
 
           named_route( singular_resource_name, singular_resource_name + "/:id", { :controller => plural_resource_name, :id => nil, :grammatical_number => 'singular' } )
           named_route( plural_resource_name, plural_resource_name + "/:ids", { :controller => plural_resource_name, :ids => nil, :grammatical_number => 'plural' } )
+          named_route( 'formatted_' + singular_resource_name, singular_resource_name + "/:id.:format", { :controller => plural_resource_name, :id => nil, :grammatical_number => 'singular' } )
+          named_route( 'formatted_' + plural_resource_name, plural_resource_name + "/:ids.:format", { :controller => plural_resource_name, :id => nil, :grammatical_number => 'plural' } )
+          named_route( 'formatted_without_id_' + singular_resource_name, singular_resource_name + ".:format", { :controller => plural_resource_name, :grammatical_number => 'singular' } )
+          named_route( 'formatted_without_ids' + plural_resource_name, plural_resource_name + ".:format", { :controller => plural_resource_name, :grammatical_number => 'plural' } )
 
           %w( get put post delete ).each do |method|
             named_route( method + "_" + singular_resource_name, singular_resource_name + "/:id", { :controller => plural_resource_name, :id => nil, :grammatical_number => 'singular', :conditions => { :method => method } )
